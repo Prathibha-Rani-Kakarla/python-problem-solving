62956c5ab6661a8858f79bd7bc4396010a2baa0f
Prathibha-Rani-Kakarla <kakarlaprathibha7@gmail.com>
2024-08-05 13:50:36 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,29 @@
+
+votes = [
+       'Tony', 'Bucky', 'Rogers', 'Damon', 'Stefan',
+       'Bucky', 'Rogers', 'Rogers', 'Tony', 'Damon',
+      'Elena', 'Stefan', 'Tony', 'Thanos', 'Elena',
+       'Thanos', 'Matt', 'Rose', 'Thanos', 'Damon'
+]
+age = {
+    'Tony': 61,
+    'Bucky': 54,
+    'Stefan': 79,
+    'Rogers': 80,
+    'Vicky': 34,
+    'Thanos': 50,
+    'Matt': 55,
+    'Rose': 42,
+    'Elena': 70,
+    'Damon': 78
+}
+
+emails = [
+    'john.deer@gmail.com',
+    'alex.barner@yahoo.com',
+    'brad.cooper@hotmail.com',
+    'cindy.barner@hotmail.com',
+    'matt.damon@gmail.com',
+    'george.cloony@yahoo.com',
+    'mc.barner@hotmail.com'
+]
